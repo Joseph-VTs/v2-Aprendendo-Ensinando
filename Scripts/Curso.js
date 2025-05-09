b3ca6c5b7e3485b74e3e5c28253e1437f366c385
Joseph-VTs <compcopycode@gmail.com>
2025-05-08 22:12:19 -0300

    Revert "Update Curso.js"
    This reverts commit 1b063060d1b364af28a8cfe4bfe97940c7f7bc7f.

--- Scripts/Curso.js
+++ Scripts/Curso.js
@@ -16,18 +16,3 @@ document.addEventListener("DOMContentLoaded", function () {
     observer.observe(video);
   });
 });
-
-document.addEventListener("DOMContentLoaded", () => {
-  let video = document.querySelector("video");
-  
-  function checkPlayback() {
-    if (video.currentTime >= 0.1) {
-      video.currentTime = 0; // Reinicia de forma suave
-      video.play();
-    }
-    requestAnimationFrame(checkPlayback);
-  }
-
-  video.play();
-  requestAnimationFrame(checkPlayback);
-});
